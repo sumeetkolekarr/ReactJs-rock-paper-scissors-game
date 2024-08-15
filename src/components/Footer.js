995818a02691d836b3eb2dcf8f3c323d34f2f769
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2024-08-15 11:58:08 +0530

    Some Changes from my side

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,13 +10,13 @@ const Footer = () => {
   return (
     <>
       <footer className="footer">
-        <div class="attribution">
+        {/* <div class="attribution">
           Challenge by{" "}
           <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
             Frontend Mentor
           </a>
           . Coded by <a href="https://youtube.com/codebucks" target="_blank">CodeBucks</a>.
-        </div>
+        </div> */}
         <button className="rules" onClick={toggle}>
           Rules
         </button>
